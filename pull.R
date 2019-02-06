bc080e8bf608f35474af3d70bf9f2772effa37a5
Rommel <chaosmembrane@gmail.com>
2019-02-07 01:59:56 +0800

    added save.image and setwd()

--- pull.R
+++ pull.R
@@ -1,5 +1,9 @@
 library(googlesheets)
+setwd("/Users/rpanal/Documents/R/rtestprojs")
+
 gskey <- "1w4awm9PWPFBjlN_Uijd2vmg3_bAxczmHiss9MJsLuv8"
 PL_list <- gs_key(gskey)
 PL <- gs_read(PL_list)
 PL <- as.data.frame(PL)
+
+save.image("rprojenv.RData")
