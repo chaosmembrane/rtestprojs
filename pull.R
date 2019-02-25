77a09c71d057fe8207712275f24902e7f395911a
Rommel <chaosmembrane@gmail.com>
2019-02-25 11:16:37 +0800

    tibble

--- pull.R
+++ pull.R
@@ -2,19 +2,21 @@
 #install.packages("googlesheets")
 #install.packages("dplyr")
 #install.packages("ggplot2")
+#install.packages("readr")
 library(dplyr)
 library(googlesheets)
 library(gapminder)
 library(ggplot2)
+library(readr)
 
 #gs_auth(new_user = TRUE)
 #setwd("/Users/rpanal/Documents/R/rtestprojs")
-#setwd("C:/Users/chaos/OneDrive/Documents/rprojects/rtestprojs")
+setwd("C:/Users/chaos/OneDrive/Documents/rprojects/rtestprojs")
 
 path <- file.path("~", "rprojects", "rtestprojs")
 gskey <- "1w4awm9PWPFBjlN_Uijd2vmg3_bAxczmHiss9MJsLuv8"
 PL_list <- gs_key(gskey)
 PL <- gs_read(PL_list)
-PL <- as.data.frame(PL)
+#PL <- as.data.frame(PL)
 
 save.image("rprojenv.RData")
